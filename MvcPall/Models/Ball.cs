b2fd0d45387f6af85e430475d8ec0a8aec309837
KregorLatt <kregorlatt1221@gmail.com>
2022-09-22 09:14:11 -0700

    Add a data model class

---
+++ MvcPall/Models/Ball.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcBall.Models
+{
+    public class Ball
+    {
+        public int Id { get; set; }
+        public string color { get; set; }
+
+        
+        
+        public string Name { get; set; }
+        public int size { get; set; }
+    }
+}
