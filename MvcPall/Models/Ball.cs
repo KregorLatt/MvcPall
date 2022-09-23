13bd57c1cb5a4686d32214d16bfcd85d6e95a777
KregorLatt <kregorlatt1221@gmail.com>
2022-09-23 09:21:00 +0300

    Add validation rules to the movie model

--- MvcPall/Models/Ball.cs
+++ MvcPall/Models/Ball.cs
@@ -6,13 +6,30 @@ namespace MvcBall.Models
     public class Ball
     {
         public int Id { get; set; }
+        
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string Color { get; set; }
+        
 
-        
-        
+        [Range(1, 100)]
+        [Required]
         public string Name { get; set; }
-        public int Size { get; set; }
-        public string Players { get; set; }
+        
 
+        
+        [Required]
+        [StringLength(30)]
+        public int Size { get; set; }
+        
+
+        
+        [Required]
+        [StringLength(30)]
+        public string Players { get; set; }
+        
     }
+
+    
 }
